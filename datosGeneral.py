bd4724c57dbcd2abca7e26bff75a3e4d418c3cea
joaquinVazquez <joaquin.vazquez@unisa.edu.mx>
2025-08-04 23:14:21 -0600

    Ejercicio datos

---
+++ datosGeneral.py
@@ -0,0 +1,11 @@
+space_odyssey = 2001
+print(space_odyssey)
+
+texto = "Good night & Good luck"
+print("El tipo de dato de texto es: ", type(texto))
+
+verdad = True
+print("El tipo de dato verdad es: ", verdad)
+
+result = 10 * 3.0
+print("El contenido de result es de: ", result)
