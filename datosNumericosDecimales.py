ee9b39f4796df8c937ab02bf2f11561a658631c4
joaquinVazquez <joaquin.vazquez@unisa.edu.mx>
2025-08-04 22:51:46 -0600

    Ejercicio datos flotantes

---
+++ datosNumericosDecimales.py
@@ -0,0 +1,7 @@
+a = 10.0
+b = 12.5
+c = 3.1416
+
+print("El valor de a es: ", a, "| El tipo de dato a es: ", type(a))
+print("El valor de b es: ", b, "| El tipo de dato b es: ", type(b))
+print("El valor de c es: ", c, "| El tipo de dato c es: ", type(c))
