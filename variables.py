645df2bc01122f5d14f87dc3245803806318402b
joaquinVazquez <joaquin.vazquez@unisa.edu.mx>
2025-08-04 23:10:21 -0600

    Ejercicios variables

---
+++ variables.py
@@ -0,0 +1,24 @@
+
+# Identificadores
+nombre_alumno = "Joaquincito"
+edad_alumno = 8
+
+# Constantes
+PI = 3.1416
+DIAS_EN_SEMANA = 7
+
+
+# Variables
+calif_matematicas = 70
+calif_ciencias = 70
+
+# Mostrando informacion
+print("Indetificador- Nombre del alumno: ", nombre_alumno)
+print("Indetificador- Edad del alumno: ", edad_alumno)
+
+print("\nConstante - Valor de PI: ", PI)
+print("\nConstante - Dias en una semana: ", DIAS_EN_SEMANA)
+
+print("\nVariables - Calificacion en matematicas: ", calif_matematicas)
+print("\nVariables - Calificacion en ciencias: ", calif_ciencias)
+
