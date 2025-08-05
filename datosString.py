964e14b80feda0d1d0387fa5a905662212070fdf
joaquinVazquez <joaquin.vazquez@unisa.edu.mx>
2025-08-04 22:57:25 -0600

    Ejercicio datos String

---
+++ datosString.py
@@ -0,0 +1,10 @@
+saludo = "Hola"
+color = "Rojo"
+
+saludo_simple = "Buenos dias"
+color_simple = 'Azul'
+
+print("Valor de saludo: ", saludo, " | Tipo de dato de saludo: ", type(saludo))
+print("Valor de saludo: ", color, " | Tipo de dato de saludo: ", type(color))
+print("Valor de saludo: ", saludo_simple, " | Tipo de dato de saludo: ", type(saludo_simple))
+print("Valor de saludo: ", color_simple, " | Tipo de dato de saludo: ", type(color_simple))
