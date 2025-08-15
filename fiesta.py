b7c200b19d13bbcea6bf7fcadbb10d1c4657c2d6
joaquinVazquez <joaquin.vazquez@unisa.edu.mx>
2025-08-15 12:19:29 -0600

    Ejercicio fiesta de cumpleaños

---
+++ fiesta.py
@@ -0,0 +1,16 @@
+dinero = 500
+dineroSorpresa = 50
+
+pastel = 180
+globos = 6 * 25
+refresco = 4 * 30
+dulces = 3 * 40
+
+costoTotal = pastel + globos + refresco + dulces
+print(f"El costo total de las compras es: {costoTotal}")
+
+dineroTotal = dinero + dineroSorpresa
+print(f"El dinero total es de. {dineroTotal}")
+
+dinero_faltante_sobrante = costoTotal - dineroTotal
+print(f"El dinero faltante o sobrante es: {dinero_faltante_sobrante}")
