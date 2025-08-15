338349a66edd35546cf933f95fceddaa881a7437
joaquinVazquez <joaquin.vazquez@unisa.edu.mx>
2025-08-15 12:38:54 -0600

    Ejercicios divisiones

---
+++ division.py
@@ -0,0 +1,5 @@
+pizzaGrande = 3 
+amigos = 18
+
+pizzaPorAmigo = pizzaGrande / amigos
+print(f"Total de pizza por amigo es de {pizzaPorAmigo}")
---
+++ divisionEntera.py
@@ -0,0 +1,5 @@
+estudiantes = 85
+cupoAutobus = 12
+
+busNecesario = estudiantes // 12
+print(f"El número de autobuses necesarios son: {busNecesario}")
