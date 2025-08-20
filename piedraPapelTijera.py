c425a48767960e555fafbc68b24d4da2c302b753
joaquinVazquez <joaquin.vazquez@unisa.edu.mx>
2025-08-19 18:25:10 -0600

    Se agrego las comparaciones con if, elif, else para determinar el ganador del juego

--- piedraPapelTijera.py
+++ piedraPapelTijera.py
@@ -8,3 +8,9 @@ jugador2 = input("Elije, piedra, papel o tijera: 🎮 ")
 print(f"El jugador 1, ha elejido: ", jugador1)
 print(f"El jugador 2, ha elejido: ", jugador2)
 
+if jugador1 == jugador2:
+    print("Empate ambos eligieron lo mismo")
+elif (jugador1 == "piedra" and jugador2 == "tijera") or (jugador1 == "tijera" and jugador2 == "papel") or (jugador1 == "papel" and jugador2 == "piedra"):
+    print("Jugador uno es el ganador")
+else:
+    print("El jugador dos, es el ganador")
